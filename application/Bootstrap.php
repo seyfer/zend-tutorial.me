21711a788bf5cca5d0c7f4243d8f3faf69e32bf5
seyfer <seyferseed@mail.ru>
2013-11-22 10:02:39 +0800

    Dev to the end

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,11 +9,21 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
         $view->doctype('XHTML1_STRICT');
     }
 
+    protected function _initAutoloader()
+    {
+        $loader = function($className) {
+            $className = str_replace('\\', '_', $className);
+            Zend_Loader_Autoloader::autoload($className);
+        };
+
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader->pushAutoloader($loader, 'Application\\');
+    }
+
 //    protected function _initDb()
 //    {
 //        $this->bootstrap('db');
 //        $db = $this->getResource('db');
 //        Zend_Registry::set('db', $db);
 //    }
-
 }
